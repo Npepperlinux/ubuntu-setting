1d964b86824eb92c16acf333f09acf1d7be2a7c1
Npepperlinux <alterlinuxer@gmail.com>
2022-01-23 11:07:25 +0900

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-alias saiy="sudo apt install -y "
-saiy vivaldi-snapshot
-saiy microsoft-edge-dev
-saiy google-chrome-stable
-saiy qview
-saiy inkscape
-saiy gimp
-saiy htop
-saiy android-tools
-saiy msttcorefonts
-saiy ffmpeg
-saiy qwinff
+sudo apt install -y vivaldi-snapshot
+sudo apt install -y microsoft-edge-dev
+sudo apt install -y google-chrome-stable
+sudo apt install -y  qview
+sudo apt install -y inkscape
+sudo apt install -y gimp
+sudo apt install -y htop
+sudo apt install -y android-tools
+sudo apt install -y msttcorefonts
+sudo apt install -y ffmpeg
+sudo apt install -y qwinff
 
 sudo add-apt-repository universal
 sudo apt update
 sudo apt install vlc
+
+sudo apt remove eog
