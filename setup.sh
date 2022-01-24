d7ecb52f0efba265101b9e0bc46224a29c6f37e0
Npepperlinux <alterlinuxer@gmail.com>
2022-01-24 18:49:39 +0900

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt install -y vivaldi-snapshot
 sudo apt install -y microsoft-edge-dev
 sudo apt install -y google-chrome-stable
-sudo apt install -y  qview
+sudo apt install -y qview
 sudo apt install -y inkscape
 sudo apt install -y gimp
 sudo apt install -y htop
@@ -10,6 +10,10 @@ sudo apt install -y android-tools
 sudo apt install -y msttcorefonts
 sudo apt install -y ffmpeg
 sudo apt install -y qwinff
+sudo apt install -y darktable
+sudo apt install -y cpu-x
+sudo apt install -y inkscape
+sudo apt install -y solaar
 
 sudo add-apt-repository universal
 sudo apt update
