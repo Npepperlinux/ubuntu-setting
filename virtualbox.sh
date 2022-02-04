c3e0a725ab5d135607a1a47408aec7355f976622
Npepperlinux <alterlinuxer@gmail.com>
2022-02-04 16:56:26 +0900

    edit virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian focal contrib" | sudo tee -a /etc/apt sources.list
+sudo echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian focal contrib" >>  /etc/apt/sources.list
 sudo apt-key add oracle_vbox_2016.asc
 sudo apt-key add oracle_vbox.asc
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
