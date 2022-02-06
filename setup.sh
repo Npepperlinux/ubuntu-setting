e9af5c3ea2cd11d4b8e8f5f7ef49afad28a1b715
Npepperlinux <alterlinuxer@gmail.com>
2022-02-06 11:58:35 +0900

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+LANG=C xdg-user-dirs-gtk-update
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true 
+
 sudo apt remove -y eog firefox
 sudo apt autoremove
 
+sudo apt install -y gnome-shell-extension-prefs 
+sudo apt install -y chrome-gnome-shell gnome-shell-extension-prefs 
 sudo apt install -y vivaldi-snapshot
 sudo apt install -y microsoft-edge-dev
 sudo apt install -y google-chrome-stable
@@ -21,10 +26,13 @@ sudo apt install -y scrcpy
 sudo add-apt-repository universal
 sudo add-apt-repository ppa:phoerious/keepassxc
 sudo add-apt-repository ppa:obsproject/obs-studio
+sudo add-apt-repository -n ppa:libreoffice/ppa
 sudo apt update
+sudo apt dist-upgrade
 sudo apt install -y vlc
 sudo apt install -y keepassxc
 sudo apt install -y obs-studio
+
 mkdir ~/.config/vivaldi-snapshot/NativeMessagingHosts
 cp ~/ubuntu-setting/org.keepassxc.keepassxc_browser.json ~/.config/vivaldi-snapshot/NativeMessagingHosts/
 
