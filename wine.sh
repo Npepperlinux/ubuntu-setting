53e68a49b9655f5942fca2d429324e838a5cffc5
Npepperlinux <alterlinuxer@gmail.com>
2022-05-05 20:23:07 +0900

    update wine.sh

--- wine.sh
+++ wine.sh
@@ -6,16 +6,16 @@ echo "3) Ubuntu 20.04 (focal) & Linux Mint 20.X"
 echo "4) Ubuntu 18.04 (bionic) & Linux Mint 19.X"
 read OS_SELECT
 case $OS_SELECT in
-	1)
+	[1])
 	OS_VERSION = "jammy"
 	;;
-	2)
+	[2])
 	OS_VERSION = "impish"
 	;;
-	3)
+	[3])
 	OS_VERSION = "focal"
 	;;
-	4)
+	[4])
 	OS_VERSION = "bionic"
 	;;
 esac
@@ -27,4 +27,3 @@ sudo mv winehq-$OS_VERSION.sources /etc/apt/sources.list.d/
 
 sudo apt update
 sudo apt install -y --install-recommends winehq-stable
-
